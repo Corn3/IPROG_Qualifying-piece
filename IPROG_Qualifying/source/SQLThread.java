fc87844f5fe543b2125ed2ca1dcd688246381dd9
Corn3 <robineliasson93@gmail.com>
2020-01-11 11:38:42 +0100

    Added condition to sqlthread to handle different sql commands.

--- IPROG_Qualifying/source/SQLThread.java
+++ IPROG_Qualifying/source/SQLThread.java
@@ -1,31 +1,38 @@
 
-public class SQLThread extends Thread {
+public class SQLThread implements Runnable {
 
 	private static final String DEFAULT_DB = "db_19762992";
 	private static final String DEFAULT_COMPUTER = "atlas.dsv.su.se";
 	private static final String USR_NAME = "usr_19762992";
 	private static final String USR_PWD = "762992";
 
-	private boolean alive;
+	private volatile boolean alive = true;
+	private boolean checkOnly;
 	private String userName;
 	private DBHandler dbHandler = new DBHandler();
-
-	public SQLThread(String userName) {
+	private Thread thread;
+	
+	public SQLThread(boolean checkOnly, String userName) {
+		this.checkOnly = checkOnly;
 		this.userName = userName;
-		alive = true;
+		
+	}
+	
+	public void stop() {
+		alive = false;
+	}
+	
+	public void start() {
 		this.start();
 	}
 
 	@Override
 	public void run() {
-		while (alive) {
-			try {
-				while (!userName.equals(null)) {
-
-				}
-				Thread.sleep(30);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		while(alive) {
+			if(checkOnly == false) {
+				
+			} else {
+				
 			}
 		}
 	}
